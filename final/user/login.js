9155a9e668f702cc68d097388dcb48a80d55a7c1
shamilbin <shamiltk029@gmail.com>
2025-04-11 12:29:14 +0530

    completed cli

---
+++ final/user/login.js
@@ -0,0 +1,13 @@
+import readline from "readline-sync"
+
+
+async function login() {
+
+    try {
+        let email = readline
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+export default login;
